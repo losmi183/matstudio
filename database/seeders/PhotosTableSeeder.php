0d479d6fe256c650ef53993547b9ebf76b59078c
Milos Glogovac <milos.glogovac@gmail.com>
2021-12-18 19:57:02 +0100

    Seeder sredjen sa novim slikama i projektima

--- database/seeders/PhotosTableSeeder.php
+++ database/seeders/PhotosTableSeeder.php
@@ -14,23 +14,164 @@ class PhotosTableSeeder extends Seeder
      */
     public function run()
     {
-        for($i=1; $i<=28; $i++) 
-        {
+        // for($i=1; $i<=28; $i++) 
+        // {
+        //     Photo::create([
+        //         'full' => 'images/projects/' . $i . '.jpg',
+        //         'thumb' => 'images/projects/' . $i . '-thumb.jpg',
+        //         'project_id' => $i,
+        //     ]);
+        //     Photo::create([
+        //         'full' => 'images/projects/' . ($i+1) . '.jpg',
+        //         'thumb' => 'images/projects/' . ($i+1) . '-thumb.jpg',
+        //         'project_id' => $i,
+        //     ]);
+        //     Photo::create([
+        //         'full' => 'images/projects/' . ($i+2) . '.jpg',
+        //         'thumb' => 'images/projects/' . ($i+2) . '-thumb.jpg',
+        //         'project_id' => $i,
+        //     ]);
+        // }
+
             Photo::create([
-                'full' => 'images/projects/' . $i . '.jpg',
-                'thumb' => 'images/projects/' . $i . '-thumb.jpg',
-                'project_id' => $i,
+                'full' => 'images/projects/1 Project Vila on a Clif 1.jpg',
+                'thumb' => 'images/projects/1 Project Vila on a Clif 1.jpg',
+                'project_id' => 1,
             ]);
             Photo::create([
-                'full' => 'images/projects/' . ($i+1) . '.jpg',
-                'thumb' => 'images/projects/' . ($i+1) . '-thumb.jpg',
-                'project_id' => $i,
+                'full' => 'images/projects/1 Project Vila on a Clif 2.jpg',
+                'thumb' => 'images/projects/1 Project Vila on a Clif 2.jpg',
+                'project_id' => 1,
             ]);
             Photo::create([
-                'full' => 'images/projects/' . ($i+2) . '.jpg',
-                'thumb' => 'images/projects/' . ($i+2) . '-thumb.jpg',
-                'project_id' => $i,
+                'full' => 'images/projects/1 Project Vila on a Clif 3.jpg',
+                'thumb' => 'images/projects/1 Project Vila on a Clif 3.jpg',
+                'project_id' => 1,
             ]);
-        }
+
+            Photo::create([
+                'full' => '2 Competition Conference center 1.jpg',
+                'thumb' => '2 Competition Conference center 1.jpg',
+                'project_id' => 2,
+            ]);
+            Photo::create([
+                'full' => '2 Competition Conference center 2.jpg',
+                'thumb' => '2 Competition Conference center 2.jpg',
+                'project_id' => 2,
+            ]);
+
+            Photo::create([
+                'full' => '3 Project pavilion 1.jpg',
+                'thumb' => '3 Project pavilion 1.jpg',
+                'project_id' => 3,
+            ]);
+            Photo::create([
+                'full' => '3 Project pavilion 2.jpg',
+                'thumb' => '3 Project pavilion 2.jpg',
+                'project_id' => 3,
+            ]);
+
+            Photo::create([
+                'full' => '4 VILLA V 1.jpg',
+                'thumb' => '4 VILLA V 1.jpg',
+                'project_id' => 4,
+            ]);
+            Photo::create([
+                'full' => '4 VILLA V 2.jpg',
+                'thumb' => '4 VILLA V 2.jpg',
+                'project_id' => 4,
+            ]);
+            Photo::create([
+                'full' => '4 VILLA V 3.jpg',
+                'thumb' => '4 VILLA V 3.jpg',
+                'project_id' => 4,
+            ]);
+
+            Photo::create([
+                'full' => '5 Apartment building 1.jpg',
+                'thumb' => '5 Apartment building 1.jpg',
+                'project_id' => 5,
+            ]);
+            Photo::create([
+                'full' => '5 Apartment building 2.jpg',
+                'thumb' => '5 Apartment building 2.jpg',
+                'project_id' => 5,
+            ]);
+
+
+            
+            Photo::create([
+                'full' => '6 CREATIVE-INNOVATIVE MULTIFUNCTIONAL CENTER LOŽIONICA 1.jpg',
+                'thumb' => '6 CREATIVE-INNOVATIVE MULTIFUNCTIONAL CENTER LOŽIONICA 1.jpg',
+                'project_id' => 6,
+            ]);
+            Photo::create([
+                'full' => '6 CREATIVE-INNOVATIVE MULTIFUNCTIONAL CENTER LOŽIONICA 2.jpg',
+                'thumb' => '6 CREATIVE-INNOVATIVE MULTIFUNCTIONAL CENTER LOŽIONICA 2.jpg',
+                'project_id' => 6,
+            ]);
+            Photo::create([
+                'full' => '6 CREATIVE-INNOVATIVE MULTIFUNCTIONAL CENTER LOŽIONICA 1.jpg',
+                'thumb' => '6 CREATIVE-INNOVATIVE MULTIFUNCTIONAL CENTER LOŽIONICA 1.jpg',
+                'project_id' => 6,
+            ]);
+            Photo::create([
+                'full' => '6 CREATIVE-INNOVATIVE MULTIFUNCTIONAL CENTER LOŽIONICA 2.jpg',
+                'thumb' => '6 CREATIVE-INNOVATIVE MULTIFUNCTIONAL CENTER LOŽIONICA 2.jpg',
+                'project_id' => 6,
+            ]);
+            Photo::create([
+                'full' => '6 CREATIVE-INNOVATIVE MULTIFUNCTIONAL CENTER LOŽIONICA 2.jpg',
+                'thumb' => '6 CREATIVE-INNOVATIVE MULTIFUNCTIONAL CENTER LOŽIONICA 2.jpg',
+                'project_id' => 6,
+            ]);
+
+
+            Photo::create([
+                'full' => '7 Competition  for the Pavilion and Depot Extension of the Museum of Contemporary Art .jpg',
+                'thumb' => '7 Competition  for the Pavilion and Depot Extension of the Museum of Contemporary Art .jpg',
+                'project_id' => 7,
+            ]);
+            Photo::create([
+                'full' => '7 Competition  for the Pavilion and Depot Extension of the Museum of Contemporary Art 2.jpg',
+                'thumb' => '7 Competition  for the Pavilion and Depot Extension of the Museum of Contemporary Art 2.jpg',
+                'project_id' => 7,
+            ]);
+
+
+            Photo::create([
+                'full' => '8 Interior design project.jpg',
+                'thumb' => '8 Interior design project.jpg',
+                'project_id' => 8,
+            ]);
+            Photo::create([
+                'full' => '8 Interior design project 1B.jpg',
+                'thumb' => '8 Interior design project 1B.jpg',
+                'project_id' => 8,
+            ]);
+            Photo::create([
+                'full' => '8 Interior design project 2.jpg',
+                'thumb' => '8 Interior design project 2.jpg',
+                'project_id' => 8,
+            ]);
+
+
+            Photo::create([
+                'full' => '9 Mediterranean project located in Igalo Montenegro 01.jpg',
+                'thumb' => '9 Mediterranean project located in Igalo Montenegro 01.jpg',
+                'project_id' => 9,
+            ]);
+            Photo::create([
+                'full' => '9 Mediterranean project located in Igalo Montenegro 02.jpg',
+                'thumb' => '9 Mediterranean project located in Igalo Montenegro 02.jpg',
+                'project_id' => 9,
+            ]);
+            Photo::create([
+                'full' => '9 Mediterranean project located in Igalo Montenegro 03.jpg',
+                'thumb' => '9 Mediterranean project located in Igalo Montenegro 03.jpg',
+                'project_id' => 9,
+            ]);
+
+
     }
 }
--- database/seeders/ProjectsTableSeeder.php
+++ database/seeders/ProjectsTableSeeder.php
@@ -14,16 +14,83 @@ class ProjectsTableSeeder extends Seeder
      */
     public function run()
     {
-        for($i=1; $i<=28; $i++) 
-        {
-            Project::create([
-                'order' => $i,
-                'name' => 'Project' . $i,
-                'description' => 'Hallway visualization done for a project previously shown on a courtyard image. This is our newest visualization project and is ongoing so stay tuned for more images coming soon.',
-                // 'image_full' => 'images/projects/'.$i.'.jpg',
-                // 'image_thumb' => 'images/projects/'.$i.'-thumb.jpg',
-            ]);
-        }
+        // for($i=1; $i<=28; $i++) 
+        // {
+        //     Project::create([
+        //         'order' => $i,
+        //         'name' => 'Project' . $i,
+        //         'description' => 'Hallway visualization done for a project previously shown on a courtyard image. This is our newest visualization project and is ongoing so stay tuned for more images coming soon.',
+        //         // 'image_full' => 'images/projects/'.$i.'.jpg',
+        //         // 'image_thumb' => 'images/projects/'.$i.'-thumb.jpg',
+        //     ]);
+        // }
+
+        Project::create([
+            'order' => 1,
+            'name' => 'Villa on a cliff',
+            'description' => '<p>Architecture done by<span class="text-danger">studio.paralele</span></p>
+            <p>Location Montenegro</p>'
+        ]);
+
+        Project::create([
+            'order' => 2,
+            'name' => '2nd prize (1st prize not awarded) ',
+            'description' => '<p>Educational-conference center of the Central Bank of Montenegro</p>
+            <p>Architecture done by<span class="text-danger">studio.paralele</span></p>
+            <p>Location Montenegro, Tivat</p>'
+        ]);
+
+        Project::create([
+            'order' => 3,
+            'name' => 'Unbuilt from the arhives. House in Portugal.',
+            'description' => 'Day end render of a courtyard on the north side of the kitchen/dining. This is a quite courtyard with a reflecting pool.'
+        ]);
+
+        Project::create([
+            'order' => 4,
+            'name' => 'VILLA V',
+            'description' => '<p>five bedroom luxury villa</p>
+            <p>Architecture done by<span class="text-danger">studio.paralele</span></p>
+            <p>Location Montenegro</p>'
+        ]);
+
+        Project::create([
+            'order' => 5,
+            'name' => '3x3 apartment building',
+            'description' => '<p>status: under construction</p>
+            <p>Architecture done by<span class="text-danger">studio.paralele</span></p>
+            <p>Location Montenegro</p>'
+        ]);
+
+        Project::create([
+            'order' => 6,
+            'name' => 'Honorable mention at the international architectural-urban competition for conceptual design',
+            'description' => '<p>CREATIVE-INNOVATIVE MULTIFUNCTIONAL CENTER "LOŽIONICA"</p>
+            <p>Architecture done by<span class="text-danger">Arhinova</span></p>
+            <p>Location Belgrade</p>'
+        ]);
+
+        Project::create([
+            'order' => 7,
+            'name' => '7 Competition  for the Pavilion and Depot Extension of the Museum of Contemporary Art',
+            'description' => '<p>7 Competition  for the Pavilion and Depot Extension of the Museum of Contemporary Art</p>
+            <p>Architecture done by<span class="text-danger">mat.studio</span> in cooperation with <span class="text-danger">space love studio</span></p>
+            <p>Location Belgrade</p>'
+        ]);
+
+        Project::create([
+            'order' => 8,
+            'name' => 'New interior design project',
+            'description' => '<p>Architecture done by<span class="text-danger">mat.studio</span></p>
+            <p>Location Kosmaj</p>'
+        ]);
+
+        Project::create([
+            'order' => 9,
+            'name' => 'New interior design project',
+            'description' => '<p>Architecture done by<span class="text-danger">mat.studio</span></p>
+            <p>Location Belgrade</p>'
+        ]);
         
     }
 }
